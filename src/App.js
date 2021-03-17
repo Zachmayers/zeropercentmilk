94c9b00be3980fb5fd519b55b3f6727771a3ce3d
Zachary <rick.jsventures.com>
2021-03-17 13:22:23 -0400

    React app source code

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo1.png';
+import title from './title.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+        <img src={logo} className="App-logo" alt="logo" />
+        
+       <img src={title}/>
+       
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
