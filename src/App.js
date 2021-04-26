c16d0adcf6a2853b3e39d9a9101fb366b44e03a0
Zachary <rick.jsventures.com>
2021-04-26 19:53:53 -0400

    uploading temporary splash page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import logo from './logo1.png';
 import title from './title.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      
-        <img src={logo} className="App-logo" alt="logo" />
-        
-       <img src={title}/>
-       
-      </header>
-      
+    <div>
+      <div class="row">
+        <div class="col">
+          <div class="container">
+            <div class="row justify-content-center">
+              <img src={logo}></img>
+            </div>
+          </div>
+        <div class="col">
+          <p class="glitch">
+            <span aria-hidden="true">Get ready for Nothing.</span>
+            Get ready for Nothing.
+            <span aria-hidden="true">Get ready for Nothing.</span>
+          </p>
+        </div> 
+        </div>
+      </div>
+      <div class="row justify-content-center">
+      <p class="glitch">
+            <span aria-hidden="true">Spring 2021</span>
+              <a href="https://water.org/">Spring 2021</a>
+            <span aria-hidden="true">Spring 2021</span>
+          </p>
+      </div>
     </div>
   );
 }
